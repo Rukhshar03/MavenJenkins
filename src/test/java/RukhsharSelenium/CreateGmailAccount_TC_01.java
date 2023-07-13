b15976c5510675a68517e80e7682006ca7d8a648
tariq <tariqjawed117@gmail.com>
2023-07-13 11:00:11 +0200

    Added jenkins files

---
+++ src/test/java/RukhsharSelenium/CreateGmailAccount_TC_01.java
@@ -0,0 +1,68 @@
+package RukhsharSelenium;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CreateGmailAccount_TC_01 extends SeleniumBase{
+	
+	SeleniumBase sb = new SeleniumBase();
+	GmailLocator gl = new GmailLocator();
+	
+	@BeforeTest
+	public void launchBrowser() throws InterruptedException {
+		
+		String url = "https://support.google.com/accounts/answer/27441?hl=en#";
+		sb.launchBrower("chrome",url);	
+	}
+	
+	@Test()
+	public void gmailAccountCreation() throws InterruptedException {
+		
+		sb.clickByXpath(gl.enterButtonMyself);
+		
+		sb.swichToWindow(1);
+		
+		String actualNameText = sb.getTextByXpath(gl.enterName);
+		String expetedNameText="Enter your name";
+		Assert.assertEquals(actualNameText,expetedNameText);
+		System.out.println("User is at Enter your name page");
+		
+		sb.enterTextByXpath(gl.enterFirstName, "Rukhshar");
+		sb.enterTextByXpath(gl.enterLastName, "Fatema");
+		
+		sb.clickByXpath(gl.nextButton);
+		
+		sb.explictWaitVisibilityofElementByLOcator(gl.textBasicInformation);
+		
+        String actualBasicInfo = sb.getTextByXpath(gl.textBasicInformation);
+		String expetedBasicInfo="Basic information";
+		Assert.assertEquals(actualBasicInfo,expetedBasicInfo);
+		System.out.println("User is at Basic information page");
+		
+		sb.selectByVisisbleText(gl.monthJanuary, "January");
+		
+        sb.enterTextByID(gl.day,"04");	
+        sb.enterTextByID(gl.year,"1995");
+		
+        sb.selectByVisisbleTextByID(gl.gender,"Female");
+		
+		sb.clickByXpath(gl.nextButton);
+		
+		sb.explictWaitVisibilityofElementByLOcator(gl.textChooseEmailAdd);
+		
+		String actualGmailAddress = sb.getTextByXpath(gl.textChooseEmailAdd);	
+	    String expetedGmailAddress="Choose your Gmail address";	
+		Assert.assertEquals(actualGmailAddress,expetedGmailAddress);
+		System.out.println("User is at Choose your Gmail address page");
+		
+		sb.clickByXpath(gl.radioButton);	
+		sb.clickByXpath(gl.nextButton);
+		
+	}
+       @AfterTest
+       public void closeBrowser(){
+    	   sb.closeBrower();	
+	  }
+
+}
---
+++ src/test/java/allreReporting/SampleProgram.java
@@ -0,0 +1,16 @@
+package allreReporting;
+
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+
+public class SampleProgram {
+	
+	@Description
+	@Test()
+	public void sample() {
+		
+	}
+
+}
